2bcbcc0bdce62715968625b9a8718d4e17ddcd94
Pedro Montero <pmontero@intecmar.gal>
2021-07-05 18:23:57 +0200

    refactor: variable names to English

--- template_gen.py
+++ template_gen.py
@@ -11,11 +11,11 @@ import logging
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-configuraciones = {}
+configurations = {}
 
 PATH = '/mnt/netapp_ext2/Store_Meteo/'
 
-configuraciones['WW3'] = {
+configurations['WW3'] = {
     'mallas'  : 
             {'atn'     : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/PRODUCTOS/THREDDS/${ano}${mes}${dia}/${ciclo}/ww3_atn_${ano}${mes}${dia}_${ciclo}00.nc'}    , 
              'iberia'  : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/PRODUCTOS/THREDDS/${ano}${mes}${dia}/${ciclo}/ww3_iberico_${ano}${mes}${dia}_${ciclo}00.nc'}, 
@@ -26,7 +26,7 @@ configuraciones['WW3'] = {
     'offset'  : 0,
 }
 
-configuraciones['ROMS'] = {
+configurations['ROMS'] = {
     'mallas'   : {'iberia' : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/PRODUCTOS/ROMS_GEOPOT/${ano}${mes}${dia}/${ciclo}/roms_002_${ano}${mes}${dia}_${ciclo}00.nc'},
                  },
     'version' : 1      ,
@@ -34,7 +34,7 @@ configuraciones['ROMS'] = {
     'offset'  : 0,
 }
 
-configuraciones['WRF'] = {
+configurations['WRF'] = {
     'mallas'   : 
                {'galicia' : {'file_template' : '%s/OP/DATOS/PRODUCTOS/METEOSIX/${ano}${mes}${dia}/${ciclo}/wrf_arw_det_history_d03_${ano}${mes}${dia}_${ciclo}00.nc4' % PATH},
                 'iberia'  : {'file_template' : '%s/OP/DATOS/PRODUCTOS/METEOSIX/${ano}${mes}${dia}/${ciclo}/wrf_arw_det_history_d02_${ano}${mes}${dia}_${ciclo}00.nc4' % PATH},
@@ -44,7 +44,7 @@ configuraciones['WRF'] = {
     'offset'  : 0,
 }
 
-configuraciones['MOHID'] = {
+configurations['MOHID'] = {
     'mallas'  : 
             {'arousa'  : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/PRODUCTOS/NCML/MyCoast/MOHID/arousa/MOHID_Arousa_${ano}${mes}${dia}_${ciclo}00.nc4'},
              'artabro' : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/PRODUCTOS/NCML/MyCoast/MOHID/artabro/MOHID_Artabro_${ano}${mes}${dia}_${ciclo}00.nc4'},
@@ -56,7 +56,7 @@ configuraciones['MOHID'] = {
     'offset'  : 1,
 }
 
-configuraciones['SWAN'] = {
+configurations['SWAN'] = {
     'mallas'   : 
        {'galicia' : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/RESULTADOS/SWAN/${ano}${mes}${dia}/${ciclo}/swan_galicia_${ano}${mes}${dia}_${ciclo}00.nc'},
         'baixas'  : {'file_template' : '/mnt/netapp_ext2/Store_Meteo/OP/DATOS/RESULTADOS/SWAN/${ano}${mes}${dia}/${ciclo}/MyCoast_baixas_${ano}${mes}${dia}_${ciclo}00.nc4'},
@@ -66,53 +66,53 @@ configuraciones['SWAN'] = {
     'offset'  : 0,
 }
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
-    # Procesado de la linea de comandos:
+    # Processing command line:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m','--modelo',help='Modelo (WW3, ROMS)',type=str)
-    parser.add_argument('-g','--grid',help='Malla (atn, iberia, galicia, )',type=str)
+    parser.add_argument('-m', '--model', help='Model (WW3, ROMS,...)', type=str)
+    parser.add_argument('-g', '--grid', help='Grid (atn, iberia, galicia,...)', ype=str)
 
-    args   = parser.parse_args()
+    args = parser.parse_args()
 
-    modelo = args.modelo.upper()
-    malla  = args.grid
+    model = args.model.upper()
+    grid = args.grid
 
-    atributos = {}
+    attributes = {}
 
-    file_template = configuraciones[modelo]['mallas'][malla]['file_template']
+    file_template = configurations[model]['mallas'][grid]['file_template']
 
-    atributos['modelo']        = modelo
-    atributos['malla']         = malla
-    atributos['file_template'] = string.Template(file_template).substitute({'ano' : '????', 'mes' : '??', 'dia' : '??', 'ciclo' : '??'})
-    atributos['date_template'] = string.Template(file_template).substitute({'ano' : '%Y', 'mes' : '%m', 'dia' : '%d', 'ciclo' : '%H'}).split('/')[-1]
-    atributos['version']       = configuraciones[modelo]['version']
-    atributos['tipo']          = configuraciones[modelo]['tipo']
+    attributes['modelo']        = model
+    attributes['malla']         = grid
+    attributes['file_template'] = string.Template(file_template).substitute({'ano' : '????', 'mes' : '??', 'dia' : '??', 'ciclo' : '??'})
+    attributes['date_template'] = string.Template(file_template).substitute({'ano' : '%Y', 'mes' : '%m', 'dia' : '%d', 'ciclo' : '%H'}).split('/')[-1]
+    attributes['version']       = configurations[model]['version']
+    attributes['tipo']          = configurations[model]['tipo']
 
-    ficheros = glob(atributos['file_template'])
+    files = glob(attributes['file_template'])
 
-    maximo   = datetime(1900,1,1)
+    maximum = datetime(1900, 1, 1)
 
-    for fichero_in in ficheros:
+    for file_in in files:
 
-        inicio                   = datetime.strptime(fichero_in.split('/')[-1], atributos['date_template'])
-        maximo                   = max(inicio, maximo)
-        inicio_real              = inicio + timedelta(days=configuraciones[modelo]['offset'])
-        atributos['ciclo' ]      = inicio.strftime('%H')    
-        atributos['inicio']      = inicio.strftime('%Y%m%d')
-        atributos['inicio_real'] = inicio_real.strftime('%Y%m%d')
+        inicio                   = datetime.strptime(file_in.split('/')[-1], attributes['date_template'])
+        maximum                   = max(inicio, maximum)
+        inicio_real              = inicio + timedelta(days=configurations[model]['offset'])
+        attributes['ciclo']      = inicio.strftime('%H')
+        attributes['inicio']      = inicio.strftime('%Y%m%d')
+        attributes['inicio_real'] = inicio_real.strftime('%Y%m%d')
 
-        fichero_out = 'MyCOAST_V%(version)i_MeteoGalicia_%(modelo)s_%(malla)s_01hr_%(inicio_real)s%(ciclo)s_%(tipo)s.ncml' % atributos
+        file_out = 'MyCOAST_V%(version)i_MeteoGalicia_%(modelo)s_%(malla)s_01hr_%(inicio_real)s%(ciclo)s_%(tipo)s.ncml' % attributes
 
         logging.info('Procesando fecha: %s' % inicio.strftime('%Y%m%d %H'))
 
-        ncml_template = 'template.%s.%s' % (modelo, malla)
+        ncml_template = 'template.%s.%s' % (model, grid)
 
         f = open(ncml_template,'r')
         INPUT  = string.Template(f.read())
         f.close()
 
-        f = open('%s/%s/%s' % (modelo, malla, fichero_out),'w')
+        f = open('%s/%s/%s' % (model, grid, file_out), 'w')
         resultado = INPUT.substitute(ano   = inicio.strftime('%Y'),
                                      mes   = inicio.strftime('%m'),
                                      dia   = inicio.strftime('%d'),
@@ -124,20 +124,20 @@ if __name__=='__main__':
         f.write(resultado)
         f.close()
 
-    logging.info('Last: %s' % maximo.strftime('%Y%m%d %H%M'))
+    logging.info('Last: %s' % maximum.strftime('%Y%m%d %H%M'))
 
     f = open(ncml_template,'r')
     INPUT  = string.Template(f.read())
     f.close()
 
-    f = open('%s/%s/%s' % (modelo, malla, 'latest.ncml'),'w')
-    resultado = INPUT.substitute(ano   = maximo.strftime('%Y'),
-                                 mes   = maximo.strftime('%m'),
-                                 dia   = maximo.strftime('%d'),
-                                 ANO   = maximo.strftime('%Y'),
-                                 MES   = maximo.strftime('%m'),
-                                 DIA   = maximo.strftime('%d'),
-                                 ciclo = maximo.strftime('%H'),
+    f = open('%s/%s/%s' % (model, grid, 'latest.ncml'), 'w')
+    resultado = INPUT.substitute(ano   = maximum.strftime('%Y'),
+                                 mes   = maximum.strftime('%m'),
+                                 dia   = maximum.strftime('%d'),
+                                 ANO   = maximum.strftime('%Y'),
+                                 MES   = maximum.strftime('%m'),
+                                 DIA   = maximum.strftime('%d'),
+                                 ciclo = maximum.strftime('%H'),
                                  )
     f.write(resultado)
     f.close()
